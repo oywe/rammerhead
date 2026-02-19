9147600b01729e02a01adc98acbbd6209779fad2
oywe <savory_pretax.20@icloud.com>
2026-02-20 01:08:46 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -19,7 +19,7 @@ module.exports = {
 
     bindingAddress: '0.0.0.0',
     port: process.env.PORT || 8080,
-    crossDomainPort: process.env.PORT || 8080,
+    crossDomainPort: process.env.PORT || 8081,
     publicDir: path.join(__dirname, '../public'), // set to null to disable
 
     // enable or disable multithreading
